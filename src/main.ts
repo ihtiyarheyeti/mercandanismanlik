d509a231b0624bdd48f5b3dedce79a7df1a00832
Mercan Danismanlik <mercandanismanlik48@gmail.com>
2025-03-14 12:56:55 +0300

    Sanctum CSRF cookie ayarları güncellendi

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,13 @@ import { loadFull } from 'tsparticles'
 import Particles from 'vue3-particles'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://mercandanismanlik.com/api'
+axios.defaults.baseURL = 'https://mercandanismanlik.com'
 axios.defaults.withCredentials = true
+
+// Sanctum CSRF cookie
+axios.get('/sanctum/csrf-cookie').catch(error => {
+  console.error('CSRF cookie error:', error)
+})
 
 const app = createApp(App)
 
