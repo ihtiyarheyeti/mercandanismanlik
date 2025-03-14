c7165576c8b15e3d56a03f616e179cf6a77bb30e
Mercan Danismanlik <mercandanismanlik48@gmail.com>
2025-03-14 13:11:42 +0300

    CORS başlıkları güncellendi

--- backend/app/Http/Middleware/ForceJsonResponse.php
+++ backend/app/Http/Middleware/ForceJsonResponse.php
@@ -20,7 +20,7 @@ class ForceJsonResponse
         $response->headers->set('Content-Type', 'application/json');
 
         // CORS başlıklarını ekle
-        $response->headers->set('Access-Control-Allow-Origin', 'https://mercandanismanlik.com');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://mercan.ltd');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
